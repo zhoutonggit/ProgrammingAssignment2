2755134386e13dcb7de6b2d72265a30910a780ad
Joe Tonggit config --global user.name Joe <zhoutong@acadiau.ca>
2014-10-26 12:41:58 -0300

    Commit for R programming assignment 2 with Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An R function to cache potentially time-consuming computations for the inverse of a matrix
 
-## Write a short comment describing this function
+## Return a list of four elements, set the matrix, get the matrix, set the inverse matrix, and 
+## get the inverse matrix. If a new matrix is the input, the inverse will be NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), ...) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinvers <- function(invers) inv <<- invers
+    getinvers <- function() inv
+    list(set = set, get = get,
+         setinvers = setinvers,
+         getinvers = getinvers)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of the matirx. Based on the function of makeCacheMatrix(), if the inv is not NULL,
+## it will return the inverse matrix from the list and skip the calculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinvers()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinvers(inv)
+    inv
 }
